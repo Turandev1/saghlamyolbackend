839acfae60f0dec5c51029378d408c5667c83efe
Batur166 <baturturan2@gmail.com>
2025-08-05 20:40:46 +0400

    'huygtfrxs'

--- models/Food.js
+++ models/Food.js
@@ -19,11 +19,18 @@ const makrobesinschema = new mongoose.Schema({
 });
 
 const besindegerlerischema = new mongoose.Schema({
-  basemiqdar:{type:Number,required:true, default:100},
+  basemiqdar: { type: Number, required: true, default: 100 },
   kalori: altbilesenschema,
   makrobesinler: makrobesinschema,
   vitaminler: [{ ad: String, miktar: Number, birim: String }],
   mineraller: [{ ad: String, miktar: Number, birim: String }],
+  icindekiler: [
+    {
+      ad: { type: String, required: false },
+      miktar: { type: Number, required: false },
+      malzemeresmi: { type: String, required: false}, // Malzeme resmi varsayılan olarak "g"
+    },
+  ],
 });
 
 const porsiyonschema = new mongoose.Schema({
@@ -33,8 +40,6 @@ const porsiyonschema = new mongoose.Schema({
   aciklama: String,
 });
 
-
-
 const yiyecekschema = new mongoose.Schema({
   yiyecekadi: { type: String, unique: true },
   kategori: String,
@@ -43,17 +48,15 @@ const yiyecekschema = new mongoose.Schema({
   resimUrl: String,
   // Yeni eklenen glisemik indeks alanı
   xolesterol: { type: Number, required: false }, // Xolesterol miktarı, varsayılan olarak null
-  aciqlama:{type:String,default:"",required:false},
+  aciqlama: { type: String, default: "", required: false },
   glisemikIndeks: {
     type: String,
-    enum: ['Yoxdur',"Aşağı", "Orta", "Yüksək"], // Allow these values or null if not applicable
+    enum: ["Yoxdur", "Aşağı", "Orta", "Yüksək"], // Allow these values or null if not applicable
     default: null,
   },
   olusturulmatarihi: Date,
   songuncellenmetarihi: Date,
 });
-
-
 
 yiyecekschema.plugin(mongoosepaginate);
 module.exports = mongoose.model("Yiyecek", yiyecekschema);
